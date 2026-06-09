74db6e3f0c485270832e69b9791ed6b72d02cc36
Kartikey <kg704925@gmail.com>
2026-06-09 10:40:25 +0530

    MongoDB connection and Notes API setUp

--- Project3/src/app.js
+++ Project3/src/app.js
@@ -12,20 +12,42 @@ app.post("/notes" , async (req , res) => {
         title: data.title,
         description: data.description,
     })
-    res.status(201).json({
+    res.status(200).json({
         message : "Notes created successfully"
     })
 })
 // GET /notes => get all notes 
-app.get("./notes" , async (req , res) =>{
+app.get("/notes" , async (req , res) =>{
     const notes = await noteModel.find();
-    res.status(201).json({
+    res.status(200).json({
         message: "Notes fetched Successfully",
         notes: notes
-
     })
 })
+//DELETE /notes/:index => delete a note by index
+app.delete("/notes/:id" , async (req, res) =>{
+    const id = req.params.id;
+    await noteModel.findByIdAndDelete({
+        _id : id
+    });
+    res.status(200).json({
+        message : "Note dleted Successfully"
+    })
+})
+//PATCH /notes/:index => update a note by index
+app.patch("/notes/:id"  , async (req,res)=>{
+    const id = req.params.id;
+    const description = req.body.description;
 
+    await noteModel.findByIdAndUpdate({
+        _id : id
+    } , {
+        description : description
+    });
+    res.status(200).json({
+        message : "Notes Update Successfully"
+    })
+})
 
 
 
